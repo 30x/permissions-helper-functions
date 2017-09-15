7d3ea6fd2beb4ec16ef58194b149227fbef4c815
Martin Nally <mnally@google.com>
2017-09-14 20:58:26 -0700

    add support for creating and delting directories and entries

--- permissions-helper-functions.js
+++ permissions-helper-functions.js
@@ -3,8 +3,15 @@ const url = require('url')
 const lib = require('@apigee/http-helper-functions')
 const rLib = require('@apigee/response-helper-functions')
 
-const PERMISSIONS_URL = process.env.PERMISSIONS_URL || ""
+const PERMISSIONS_BASE = process.env.PERMISSIONS_BASE || ""
 
+function permissionsServiceUrl(possiblyRelativeURL) {
+  if (PERMISSIONS_BASE)
+    return url.resolve(PERMISSIONS_BASE, possiblyRelativeURL)
+  else
+    return possiblyRelativeURL
+}
+  
 function Permissions(permissions) {
   this.permissions = permissions
 }
@@ -26,30 +33,6 @@ Permissions.prototype.resolveRelativeURLs = function(baseURL) {
 
 function createPermissionsThen(flowThroughHeaders, res, resourceURL, permissions, callback, errorCallback) {
   var user = lib.getUser(flowThroughHeaders.authorization)
-  function processResponse(clientRes) {
-    lib.getClientResponseBody(clientRes, (body) => {
-      if (clientRes.statusCode == 201) {
-        body = JSON.parse(body)
-        lib.internalizeURLs(body, flowThroughHeaders.host)
-        callback(null, clientRes.headers.location, body, clientRes.headers)
-      } else if (errorCallback)
-        errorCallback(clientRes.statusCode, body)
-      else if (clientRes.statusCode == 400)
-        rLib.badRequest(res, body)
-      else if (clientRes.statusCode == 403)
-        rLib.forbidden(res, {
-          msg:'Forbidden. Unable to create permissions. You may not be allowed to inherit permissions from sharingSets', 
-          component: process.env.COMPONENT_NAME, 
-          subject: permissions._subject, 
-          sharingSets: permissions._inheritsPermissionsOf, 
-          user: user
-        })
-      else if (clientRes.statusCode == 409)
-        rLib.duplicate(res, body)
-      else
-        rLib.internalError(res, {statusCode: clientRes.statusCode, msg: `failed to create permissions for ${resourceURL} statusCode ${clientRes.statusCode} message ${JSON.stringify(body)}`})
-    })
-  }
   if (user == null)
     rLib.unauthorized(res)
   else {
@@ -64,35 +47,33 @@ function createPermissionsThen(flowThroughHeaders, res, resourceURL, permissions
         return rLib.badRequest(res, `permissions for ${resourceURL} must specify inheritance or at least one governor`)
     }
     var postData = JSON.stringify(permissions)
-    if (PERMISSIONS_URL !== "")
-      lib.sendExternalRequestThen(res, 'POST', PERMISSIONS_URL + '/az-permissions',  flowThroughHeaders, postData, function (clientRes) {
-        processResponse(clientRes)
+    lib.sendInternalRequestThen(res, 'POST', permissionsServiceUrl('/az-permissions'),  flowThroughHeaders, postData, function (clientRes) {
+      lib.getClientResponseObject(res, clientRes, null, (body) => {
+        if (clientRes.statusCode == 201) {
+          lib.internalizeURLs(body, flowThroughHeaders.host)
+          callback(null, clientRes.headers.location, body, clientRes.headers)
+        } else if (errorCallback)
+          errorCallback(clientRes.statusCode, body)
+        else if (clientRes.statusCode == 400)
+          rLib.badRequest(res, body)
+        else if (clientRes.statusCode == 403)
+          rLib.forbidden(res, {
+            msg:'Forbidden. Unable to create permissions. You may not be allowed to inherit permissions from sharingSets', 
+            component: process.env.COMPONENT_NAME, 
+            subject: permissions._subject, 
+            sharingSets: permissions._inheritsPermissionsOf, 
+            user: user
+          })
+        else if (clientRes.statusCode == 409)
+          rLib.duplicate(res, body)
+        else
+          rLib.internalError(res, {statusCode: clientRes.statusCode, msg: 'permissions-helper-function::createPermissionsThen failed to create permissions', resource: resourceURL, statusCode: clientRes.statusCode, body: body, permissionsUrl:  permissionsServiceUrl(`/az-permissions?${resourceURL}`)})
       })
-    else
-      lib.sendInternalRequestThen(res, 'POST', '/az-permissions',  flowThroughHeaders, postData, function (clientRes) {
-        processResponse(clientRes)
-      })
+    })
   }
 }
 
 function createTeamThen(flowThroughHeaders, res, team, callback, errorCallback) {
-  function processResponse(clientRes) {
-    lib.getClientResponseObject(res, clientRes, null, (body) => {
-      if (clientRes.statusCode == 201) {
-        lib.internalizeURLs(body, flowThroughHeaders.host)
-        callback(clientRes.headers.location, body)
-      } else if (errorCallback)
-        errorCallback(clientRes.statusCode, body)
-      else if (clientRes.statusCode == 400)
-        rLib.badRequest(res, body)
-      else if (clientRes.statusCode == 403)
-        rLib.forbidden(res, {msg: 'Forbidden. unable to create team', component: process.env.COMPONENT_NAME, reason: body})
-      else if (clientRes.statusCode == 409)
-        rLib.duplicate(res, body)
-      else
-        rLib.internalError(res, {statusCode: clientRes.statusCode, msg: 'failed to create team', body: body})
-    })
-  }
   var user = lib.getUser(flowThroughHeaders.authorization)
   if (user == null)
     rLib.unauthorized(res)
@@ -100,34 +81,70 @@ function createTeamThen(flowThroughHeaders, res, team, callback, errorCallback) 
     if (team === null || team === undefined)
       return rLib.badRequest(res, `may not set null permissions: ${resourceURL}`)
     var postData = JSON.stringify(team)
-    if (PERMISSIONS_URL !== "")
-      lib.sendExternalRequestThen(res, 'POST', PERMISSIONS_URL + '/az-teams',  flowThroughHeaders, postData, function (clientRes) {
-        processResponse(clientRes)
+    console.log('\n\n', permissionsServiceUrl('/az-teams'), '\n\n')
+    lib.sendInternalRequestThen(res, 'POST', permissionsServiceUrl('/az-teams'),  flowThroughHeaders, postData, function (clientRes) {
+      lib.getClientResponseObject(res, clientRes, null, (body) => {
+        if (clientRes.statusCode == 201) {
+          lib.internalizeURLs(body, flowThroughHeaders.host)
+          callback(clientRes.headers.location, body)
+        } else if (errorCallback)
+          errorCallback(clientRes.statusCode, body)
+        else if (clientRes.statusCode == 400)
+          rLib.badRequest(res, body)
+        else if (clientRes.statusCode == 403)
+          rLib.forbidden(res, {msg: 'Forbidden. unable to create team', component: process.env.COMPONENT_NAME, reason: body})
+        else if (clientRes.statusCode == 409)
+          rLib.duplicate(res, body)
+        else
+          rLib.internalError(res, {statusCode: clientRes.statusCode, msg: 'permissions-helper-functions::createTeamThen failed to create team', body: body})
       })
-    else
-      lib.sendInternalRequestThen(res, 'POST', '/az-teams',  flowThroughHeaders, postData, function (clientRes) {
-        processResponse(clientRes)
-      })
+    })
   }
 }
 
 function deletePermissionsThen(flowThroughHeaders, res, resourceURL, callback) {
-  function processResponse(clientRes) {
+  lib.sendInternalRequestThen(res, 'DELETE', permissionsServiceUrl(`/az-permissions?${resourceURL}`), flowThroughHeaders, undefined, function (clientRes) {
     lib.getClientResponseBody(clientRes, (body) => {
       if (clientRes.statusCode == 200)
         callback()
       else
         rLib.internalError(res, `unable to delete permissions for ${resourceURL} statusCode: ${clientRes.statusCode} text: ${body}`)
     })
-  }
-  if (PERMISSIONS_URL !== "")
-    lib.sendExternalRequestThen(res, 'DELETE', PERMISSIONS_URL + `/az-permissions?${resourceURL}`, flowThroughHeaders, undefined, function (clientRes) {
-      processResponse(clientRes)
+  })
+}
+
+function deleteTeamThen(flowThroughHeaders, res, resourceURL, callback) {
+  lib.sendInternalRequestThen(res, 'DELETE', permissionsServiceUrl(resourceURL), flowThroughHeaders, undefined, function (clientRes) {
+    lib.getClientResponseBody(clientRes, (body) => {
+      if (clientRes.statusCode == 200)
+        callback()
+      else
+        rLib.internalError(res, `unable to delete team ${resourceURL} statusCode: ${clientRes.statusCode} text: ${body}`)
     })
+  })
+}
+
+function createEntryThen(flowThroughHeaders, res, entry, callback) {
+  lib.postToInternalResourceThen(res, permissionsServiceUrl('/dir-entries'), flowThroughHeaders, entry, callback)
+}
+
+function withDirectoryEntryDo(flowThroughHeaders, res, path, callback) {
+  let searchUrl = permissionsServiceUrl(`/dir-entries?${path}`)
+  lib.withInternalResourceDo(res, searchUrl, flowThroughHeaders, callback)
+}
+
+function deleteEntryThen(flowThroughHeaders, res, directory, name, resource, callback) {
+  if (!directory)
+    return rLib.internalError(res, {msg: 'permissions-helper-functions::deleteEntryThen must provide directory and name or directory and resource', directory: directory, name: name, resource: resource})
+  let qs = `directory=${directory}`
+  if (name)
+    qs += `&name=${name}`
+  else if (resource)
+    qs += `&resource=${resource}`
   else
-    lib.sendInternalRequestThen(res, 'DELETE', `/az-permissions?${resourceURL}`, flowThroughHeaders, undefined, function (clientRes) {
-      processResponse(clientRes)
-    })
+    return rLib.internalError(res, {msg: 'permissions-helper-functions::deleteEntryThen must provide directory and name or directory and resource', directory: directory, name: name, resource: resource})
+  let searchUrl = permissionsServiceUrl(`/dir-entries?${qs}`)
+  lib.withInternalResourceDo(res, searchUrl, flowThroughHeaders, callback)
 }
 
 function withAllowedDo(headers, res, resourceURL, property, action, base, path, callback, withScopes) {
@@ -137,31 +154,32 @@ function withAllowedDo(headers, res, resourceURL, property, action, base, path, 
   var user = lib.getUser(headers.authorization)
   var resourceURLs = Array.isArray(resourceURL) ? resourceURL : [resourceURL]
   var qs = resourceURLs.map(x => `resource=${x}`).join('&')
-  var permissionsURL = `/az-is-allowed?${qs}`
+  var permissionsUrl = `/az-is-allowed?${qs}`
   if (user != null)
-    permissionsURL += '&user=' + user.replace('#', '%23')
+    permissionsUrl += '&user=' + user.replace('#', '%23')
   if (action != null)
-    permissionsURL += '&action=' + action
+    permissionsUrl += '&action=' + action
   if (property != null)
-    permissionsURL += '&property=' + property
+    permissionsUrl += '&property=' + property
   if (base != null)
-    permissionsURL += '&base=' + base
+    permissionsUrl += '&base=' + base
   if (path != null)
-    permissionsURL += '&path=' + path
+    permissionsUrl += '&path=' + path
   if (withScopes != null)
-    permissionsURL += '&withScopes'
-  function processResponse(clientRes, permissionsURL) {
+    permissionsUrl += '&withScopes'
+  permissionsUrl = permissionsServiceUrl(permissionsUrl) 
+  lib.sendInternalRequestThen(res, 'GET', permissionsUrl, headers, undefined, function (clientRes) {
     lib.getClientResponseBody(clientRes, (body) => {
       try {
         body = JSON.parse(body)
       } catch (e) {
-        console.error('withAllowedDo: JSON parse failed. url:', permissionsURL, 'body:', body, 'error:', e)
+        console.error('withAllowedDo: JSON parse failed. url:', permissionsUrl, 'body:', body, 'error:', e)
       }
       var statusCode = clientRes.statusCode
       if (statusCode == 200)
         callback(body)
       else if (statusCode == 404)
-        rLib.notFound(res, {msg: `Not Found. component: ${process.env.COMPONENT_NAME} permissionsURL: ${permissionsURL}`})
+        rLib.notFound(res, {msg: `Not Found. component: ${process.env.COMPONENT_NAME} permissionsUrl: ${permissionsUrl}`})
       else if (statusCode == 401)
         rLib.unauthorized(res, body)
       else if (statusCode == 403)
@@ -169,15 +187,7 @@ function withAllowedDo(headers, res, resourceURL, property, action, base, path, 
       else
         rLib.internalError(res, `unable to retrieve withAllowedDo statusCode: ${statusCode} resourceURL: ${resourceURL} property: ${property} action: ${action} body: ${JSON.stringify(body)}`)
     })
-  }
-  if (PERMISSIONS_URL !== "")
-    lib.sendExternalRequestThen(res, 'GET', PERMISSIONS_URL + permissionsURL, headers, undefined, function (clientRes) {
-      processResponse(clientRes, PERMISSIONS_URL + permissionsURL)
-    })
-  else
-    lib.sendInternalRequestThen(res, 'GET', permissionsURL, headers, undefined, function (clientRes) {
-      processResponse(clientRes, permissionsURL)
-    })
+  })
 }
 
 function ifAllowedThen(headers, res, resourceURL, property, action, base, path, callback, withScopes) {
@@ -200,4 +210,7 @@ exports.deletePermissionsThen = deletePermissionsThen
 exports.ifAllowedThen = ifAllowedThen
 exports.withAllowedDo = withAllowedDo
 exports.createTeamThen = createTeamThen
-
+exports.deleteTeamThen = deleteTeamThen
+exports.createEntryThen = createEntryThen
+exports.deleteEntryThen = deleteEntryThen
+exports.withDirectoryEntryDo = withDirectoryEntryDo
